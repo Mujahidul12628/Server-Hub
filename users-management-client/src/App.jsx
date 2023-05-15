d873d1cbb1d31dc7376bb6ffdb219b0f5a728ae2
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-15 13:48:59 +0600

    simple crud server initialized

--- users-management-client/src/App.jsx
+++ users-management-client/src/App.jsx
@@ -31,6 +31,9 @@ function App() {
       .then(res => res.json())
       .then(data => {
         console.log('inside post response', data)
+        const newUser = [...users, data];
+        setUsers(newUser);
+        form.reset();
       })
 
 
