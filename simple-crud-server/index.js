4cff89d8a613f60771cabddc0b8d4e8ac2c03f7b
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-15 13:49:49 +0600

    Mongodb connected

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.Port || 5102;
 
 //middleware
 app.use(cors());
 app.use(express.json())
+
+// fardinihsan
+// lcOH2xOQ407Zh4Nc
+
+const uri = "mongodb+srv://fardinihsan:lcOH2xOQ407Zh4Nc@cluster0.dphnvzs.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
