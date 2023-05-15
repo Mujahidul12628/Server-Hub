a823a78da249ee7feb04cf0bea2a831ff59e37fe
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-15 18:04:23 +0600

    Simple crud client initialized

---
+++ simple-crud-client/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>CRUD Client</h1>
+
+    </div>
+  )
+}
+
+export default App
