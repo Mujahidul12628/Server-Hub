f69836fbc32ce1b84a6b91d0c50ec8a0df8aef96
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-16 13:06:57 +0600

    new user connected to Mongodb

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -4,10 +4,52 @@ import './App.css'
 
 function App() {
 
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+
+
+    // fetch('http://localhost:5102/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(user)
+    // })
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+
+    fetch('http://localhost:5102/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+
+
+    console.log(user);
+  }
+
   return (
     <div>
       <h1>CRUD Client</h1>
-
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="add user" />
+      </form>
     </div>
   )
 }
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -1,17 +1,77 @@
+// const express = require('express');
+// const cors = require('cors');
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const app = express();
+// const port = process.env.Port || 5102;
+
+// //middleware
+// app.use(cors());
+// app.use(express.json())
+
+// // fardinihsan
+// // lcOH2xOQ407Zh4Nc
+
+// const uri = "mongodb+srv://fardinihsan:lcOH2xOQ407Zh4Nc@cluster0.dphnvzs.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
+
+// async function run() {
+//     try {
+//         // Connect the client to the server	(optional starting in v4.7)
+//         await client.connect();
+
+//         const database = client.db("usersDB");
+//         const userCollection = database.collection("users");
+
+
+//         // Send a ping to confirm a successful connection
+//         await client.db("admin").command({ ping: 1 });
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+//         app.post('/users', async (req, res) => {
+//             const user = req.body;
+//             console.log('new user', user);
+//             const result = await userCollection.insertOne(user);
+//             res.send(result);
+//         })
+
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Users Management Server is running');
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server is running on Port: ${port}`)
+// })
+
 const express = require('express');
 const cors = require('cors');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.Port || 5102;
+const port = process.env.PORT || 5102;
 
-//middleware
+// Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-// fardinihsan
-// lcOH2xOQ407Zh4Nc
-
+// MongoDB connection URI
 const uri = "mongodb+srv://fardinihsan:lcOH2xOQ407Zh4Nc@cluster0.dphnvzs.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,29 +80,39 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client to the server (optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("usersDB");
+        const userCollection = database.collection("users");
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user', user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
+
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
     res.send('Users Management Server is running');
-})
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on Port: ${port}`)
-})
+    console.log(`Server is running on Port: ${port}`);
+});
