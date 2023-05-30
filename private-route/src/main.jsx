54c4b8c1273ffe721abe843ef4ce42e0d1bb50ee
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-30 16:58:59 +0600

    component added

--- private-route/src/main.jsx
+++ private-route/src/main.jsx
@@ -1,10 +1,41 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './assets/layout/Main/Main';
+import Home from './assets/pages/Home/Home';
+import Login from './assets/pages/Login/Login';
+import Register from './assets/pages/Register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+    ]
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
